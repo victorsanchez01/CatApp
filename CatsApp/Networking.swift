e0f4c7ad402938075712482357b4babd26acb24d
Victor Sanchez <vs@victorsanchez.dev>
2023-06-07 17:30:18 -0500

    Networking functions created

--- CatsApp/Networking.swift
+++ CatsApp/Networking.swift
@@ -9,4 +9,48 @@ import Foundation
 
 class Networking {
     
+    func fetchCats(completion: @escaping ([Cat]?) -> Void) {
+        guard let url = URL(string: "https://api.thecatapi.com/v1/breeds") else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print("Error: \(error)")
+                completion(nil)
+                return
+            }
+            
+            if let data = data {
+                do {
+                    let cats = try JSONDecoder().decode([Cat].self, from: data)
+                    completion(cats)
+                } catch {
+                    print("Error decoding JSON: \(error)")
+                    completion(nil)
+                }
+            }
+        }.resume()
+    }
+    
+    func fetchCatImage(for cat: Cat, completion: @escaping (Data?) -> Void) {
+        let urlString = "https://api.thecatapi.com/v1/images/search?id=\(cat.id)"
+        
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print("Error loading image: \(error)")
+                completion(nil)
+                return
+            }
+            
+            completion(data)
+        }.resume()
+    }
+    
 }
--- CatsApp/ViewModel.swift
+++ CatsApp/ViewModel.swift
@@ -5,4 +5,21 @@
 //  Created by victor.sanchez on 7/06/23.
 //
 
-import Foundation
+import SwiftUI
+
+class ViewModel {
+    @Published var cats: [Cat] = []
+    let networking = Networking()
+    
+    func fetchCats() {
+        networking.fetchCats { [weak self] cats in
+            DispatchQueue.main.async {
+                self?.cats = cats ?? []
+            }
+        }
+    }
+    
+    func fetchCatImage(for cat: Cat, completion: @escaping (Data?) -> Void) {
+        networking.fetchCatImage(for: cat, completion: completion)
+    }
+}
