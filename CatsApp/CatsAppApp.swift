7602be0bddebe267391f7af03f6bab4cbc97a28c
Victor Sanchez <vs@victorsanchez.dev>
2023-06-07 16:09:18 -0500

    Initial Commit

---
+++ CatsApp/CatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppApp.swift
+//  CatsApp
+//
+//  Created by victor.sanchez on 7/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
